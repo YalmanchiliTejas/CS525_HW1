2ba9c566754921ada966319ca3e64ab86d3aa70d
Tejas <tyalaman@purdue.edu>
2026-02-17 19:04:08 -0500

    Final changes and README

--- hw2_question6.py
+++ hw2_question6.py
@@ -7,18 +7,19 @@ import numpy as np
 import argparse
 
 
-'''
 
-generates an adjacency list to get where each of the processors has k additional connections to randomly selected processors
-'''
 
+#generates an adjacency list to get where each of the processors has k additional connections to randomly selected processors
+
+#same code as given in the assignment handout
 
 def generate_random_edges(adj, k):
 
     V = list(adj.keys())
 
     E = set()
-
+    
+    
     random.seed(0)
 
     for vertex in V:
@@ -66,9 +67,14 @@ def generate_random_edges(adj, k):
 def generate_2d_id(x, y, width):
 
     return  x*width+ y
+
+
+#generates a 2d mesh and then later on adds the extra k random edges
 def generate_2d_mesh(p, k):
     
-    proc = int(math.sqrt(p))
+    proc = int(round(math.sqrt(p)))
+
+
 
     adj = defaultdict(list)
 
@@ -89,11 +95,14 @@ def generate_2d_mesh(p, k):
                 adj[v2].append(v1)
     return generate_random_edges(adj, k)
 
+#generates an id to a processor in the 3d mesh
 def generate_3d_id(x, y, z, width):
     return x*width*width + y*width + z
 
+#generates a 3d mesh and then later on adds the extra k random edges
 def generate_3d_mesh(p, k):
-    proc = int(p ** (1/3))
+    proc = int(round(p ** (1/3)))
+
     adj = defaultdict(list)
 
     for p1 in range(proc):
@@ -119,10 +128,13 @@ def generate_3d_mesh(p, k):
     return generate_random_edges(adj, k)
 
 
+#generates a hypercube and then later on adds the extra k random edges
 def generate_hypercube(p, k):
+    
+    sixth_root = int(round(p ** (1/6)))
 
-    sixth_root = int(p ** (1/6))
     dimension = 2 * sixth_root
+    
     total_nodes = 2**(dimension)
 
     adj = defaultdict(list)
@@ -141,9 +153,9 @@ def generate_hypercube(p, k):
 
 
 
-
+#generates the bfs along with the traversal order and parents array (helpful for congestion)
 def bfs_distances(adj, src, n=None):
-    """Standard BFS shortest-path distances from src."""
+  
     if n is None:
         n = len(adj)
     dist = [-1] * n
@@ -162,12 +174,9 @@ def bfs_distances(adj, src, n=None):
     return dist, parent, order
 
 
+#estimates the diameter by sampling many source and destination pairs and then taking the max of those BFS distances
 def estimate_diameter(adj, num_pairs=10000, seed=0):
-    """
-    Simulation-based diameter estimate:
-    sample many random (s,t), compute shortest-path distance via BFS, take max.
-    More efficient variant: BFS once per sampled source and query multiple targets.
-    """
+
     random.seed(seed)
     n = len(adj)
     best = 0
@@ -175,9 +184,11 @@ def estimate_diameter(adj, num_pairs=10000, seed=0):
     # Choose about sqrt(num_pairs) sources, and for each, sample multiple targets
     num_sources = max(1, int(num_pairs**0.5))
     targets_per_source = max(1, num_pairs // num_sources)
-
+  
+    
     for _ in range(num_sources):
         s = random.randrange(n)
+        #calculate BFS distances once per every source node(more efficient that way)
         dist, _ , _= bfs_distances(adj, s, n)
 
         for _ in range(targets_per_source):
@@ -188,35 +199,14 @@ def estimate_diameter(adj, num_pairs=10000, seed=0):
     
     return best
 
-
+# estimate the bisection width by continuously sampling and partitioning into two different sets and then taking the min the edges crossing between the two partitions
 def estimate_bisection_width(adj, num_trials=10000, seed=0):
-    """
-    using native python
-    """
+ 
     random.seed(seed)
     n = len(adj.keys())
     half = n // 2
     np.random.seed(seed)
-    # vertices = list(adj.keys())
-    # best = None
 
-    # for _ in range(num_trials):
-    #     A = set(random.sample(vertices, half))
-    #     inA = [False] * n
-    #     for u in A:
-    #         inA[u] = True
-
-    #     crossings = 0
-    #     # Count each undirected edge once by counting edges from A to not-A
-    #     for u in A:
-    #         for v in adj[u]:
-    #             if not inA[v]:
-    #                 crossings += 1
-
-    #     if best is None or crossings < best:
-    #         best = crossings
-
-    # return 0 if best is None else best
 
     V = []
     U = []
@@ -226,16 +216,16 @@ def estimate_bisection_width(adj, num_trials=10000, seed=0):
             if k < vertex:
                 U.append(k)
                 V.append(vertex)
-    U = np.array(U,dtype=np.int32)
-    V = np.array(V,dtype=np.int32)
+    U = np.array(U,dtype=np.int32) # convert to nump arrays
+    V = np.array(V,dtype=np.int32) # convert to numpy arrays
 
     width = float("inf")
     for i in range(num_trials):
         side = np.zeros(n)
-        A = np.random.choice(n, size=half, replace=False)
+        A = np.random.choice(n, size=half, replace=False) # partition it into a subset
         side[A] = 1
         
-        num_crossings = int(np.count_nonzero(side[U] != side[V]))
+        num_crossings = int(np.count_nonzero(side[U] != side[V])) # find the number of crossing and then take a min
 
         width = min(width, num_crossings)
     return width if width < float("inf") else 0
@@ -245,34 +235,27 @@ def estimate_bisection_width(adj, num_trials=10000, seed=0):
 
 
 
-
+# estimates dilation and congestion
 def estimate_dilation_congestion(adj_a, adj_b,):
 
     
 
     dilation = 0
 
-    load = defaultdict(int)
+    load = defaultdict(int) # the dictionary to calculate the load on the edges
     n = len(adj_a)
 
     for s in range(len(adj_a)):
-        distance, parents, order = bfs_distances(adj_b, s, len(adj_a))
-        demand = [0] * n
+        distance, parents, order = bfs_distances(adj_b, s, len(adj_a)) #do a BFS once per every source (much more efficient)
+        demand = [0] * n  # the number of times a route passes through a vertex
         for t in adj_a[s]:
             if s < t:
                 d = distance[t]
                 if d == -1:
                     continue
-                dilation = max(dilation, d)
+                dilation = max(dilation, d) # takes the max distance from the source to all neighbors 
                 demand[t] += 1
-                # current = t
-                # while current != s:
-                #     parent = parents[current]
-                #     if parent == -1:
-                #         break
-                #     edges = (parent, current) if parent < current else (current, parent)
-                #     load[edges] += 1
-                #     current = parent
+        #Iterates up the traversal order to see which edge got most called and store those values. the max of them will be returned
         for u in reversed(order):
             p = parents[u]
             if p != -1:
@@ -283,41 +266,6 @@ def estimate_dilation_congestion(adj_a, adj_b,):
     return dilation, max(load.values()) if load else 0
 
 
-def plot_and_save_multiline(x,val_dict, x_label, y_label, title, filename):
-
-    plt.figure()
-    for key , value in val_dict.items():
-        plt.plot(x, value, marker="o", label=key)
-    plt.xlabel(x_label)
-    plt.ylabel(y_label)
-    plt.title(title)
-    plt.grid(True)
-    plt.legend()
-    plt.savefig(filename)
-    plt.close()
-
-def plot_and_save_single(x, y, x_label, y_label, title, filename):
-
-    plt.figure()
-    plt.plot(x, y, marker="o")
-    plt.xlabel(x_label)
-    plt.ylabel(y_label)
-    plt.title(title)
-    plt.grid(True)
-    plt.savefig(filename)
-    plt.close()
-
-
-def plot_diameter_bisection(diameter_dict, bisection_dict, processors):
-    
-    plot_and_save_multiline(processors, diameter_dict, "Number of Processors", "Diameter", "Diameter vs Number of Processors", "diameter.png")
-    plot_and_save_multiline(processors, bisection_dict, "Number of Processors", "Bisection Width", "Bisection Width vs Number of Processors", "bisection.png")
-
-def plot_dilation_congestion(dilation_vals, congestion_vals, processors):
-    plot_and_save_single(processors, dilation_vals, "Number of Processors", "Dilation", "Dilation vs Number of Processors", "dilation.png")
-    plot_and_save_single(processors, congestion_vals, "Number of Processors", "Congestion", "Congestion vs Number of Processors", "congestion.png")
-
-
 
 if __name__ == "__main__":
 
@@ -325,10 +273,11 @@ if __name__ == "__main__":
     args_parser = argparse.ArgumentParser(description="simulation generator")
 
     args_parser.add_argument("-m", "--mesh", type=str,help="Choose between 2D, 3D, Hypercube", default="2D", required=False)
-    # args_parser.add_argument("-p", "--processors", type=int, help="Choose the p value", default=0, required=True)
-    args_parser.add_argument("--k", "--connections", type=int,dest="k", help="Choose the number of connetions for each processor", default=4, required=False)
+    args_parser.add_argument("-p", "--processors", type=int, help="Choose the p value", default=0, required=False)
+    args_parser.add_argument("-k", "--connections", type=int,dest="k", help="Choose the number of connetions for each processor", default=4, required=False)
     args_parser.add_argument("-n", "--network_mapping",  help="network_mapping",action="store_true",  required=False)
-    args_parser.add_argument("-all", "-all_meshes", help="Run for all meshes for diameter and bisection width",action="store_true",required=False)
+    args_parser.add_argument("-all", "--all_meshes", help="Run for all meshes for diameter and bisection width",action="store_true",required=False)
+    args_parser.add_argument("-p4", "-part_4", help="part_4 code",action="store_true",required=False)
 
     processors = [2**6, 3**6, 4**6, 5**6, 6**6]
    
@@ -340,47 +289,53 @@ if __name__ == "__main__":
 
     dilation_vals = []
     congestion_vals = []
-    if not args.all:
-        for i, p in enumerate(processors):
-            if  not args.network_mapping:
-                if args.mesh == "2D":
-                    graph = generate_2d_mesh(p, args.k)
-                elif args.mesh == "3D":
-                    graph = generate_3d_mesh(p, args.k)
-                else:
-                    graph = generate_hypercube(p, args.k)
-                
-                diameter_val[args.mesh].append(estimate_diameter(graph))
-                bisection_val[args.mesh].append(estimate_bisection_width(graph))
-            else:
-                map_a = generate_2d_mesh(p, args.k)
-                map_b = generate_3d_mesh(p, args.k)
-                dilation, congestion = estimate_dilation_congestion(map_a, map_b)
-                dilation_vals.append(dilation)
-                congestion_vals.append(congestion)
-                print(f"The dilation: {dilation_vals[-1]} and congestion_vals: {congestion_vals[-1]} for this processor index:{i}")
-        
+
+    if args.p4:
+        graph_2d = generate_2d_mesh(args.processors, args.k)
+        graph_3d = generate_3d_mesh(args.processors, args.k)
+        print(f"mesh 2d has Bisection: {estimate_bisection_width(graph_2d)}\n")
+        print(f"Mesh 3d has Bisection:{estimate_bisection_width(graph_3d)}\n")
+
     else:
-        meshes = ["2D", "3D", "Hypercube"]
-        for mesh in meshes:
-            for i,  p in enumerate(processors):
-                if mesh == "2D":
-                    graph = generate_2d_mesh(p, args.k)
-                elif mesh == "3D":
-                    graph = generate_3d_mesh(p, args.k)
+
+
+        if not args.all_meshes:
+            for i, p in enumerate(processors):
+                if  not args.network_mapping:
+                    if args.mesh == "2D":
+                        graph = generate_2d_mesh(p, args.k)
+                    elif args.mesh == "3D":
+                        graph = generate_3d_mesh(p, args.k)
+                    else:
+                        graph = generate_hypercube(p, args.k)
+                    
+                    diameter_val[args.mesh].append(estimate_diameter(graph))
+                    bisection_val[args.mesh].append(estimate_bisection_width(graph))
                 else:
-                    graph = generate_hypercube(p, args.k)
-                
-                diameter_val[mesh].append(estimate_diameter(graph))
-                print(f"mesh {mesh} has diameter: {diameter_val[mesh][-1]} for this processor index: {i}")
-                bisection_val[mesh].append(estimate_bisection_width(graph))
-                print(f"mesh{mesh} has bisection: {bisection_val[mesh][-1]} for this processor index: {i}")
-    
-    if args.network_mapping:
-        plot_dilation_congestion(dilation_vals, congestion_vals, processors)
-    else:
-        plot_diameter_bisection(diameter_val, bisection_val, processors)
-        print("dimatert and bisection plotting done")
+                    map_a = generate_2d_mesh(p, args.k)
+                    map_b = generate_3d_mesh(p, args.k)
+                    dilation, congestion = estimate_dilation_congestion(map_a, map_b)
+                    dilation_vals.append(dilation)
+                    congestion_vals.append(congestion)
+                    print(f"The dilation: {dilation_vals[-1]} and congestion_vals: {congestion_vals[-1]} for this processor index:{i}")
+            
+        else:
+            meshes = ["2D", "3D", "Hypercube"]
+            for mesh in meshes:
+                for i,  p in enumerate(processors):
+                    if mesh == "2D":
+                        graph = generate_2d_mesh(p, args.k)
+                    elif mesh == "3D":
+                        graph = generate_3d_mesh(p, args.k)
+                    else:
+                        graph = generate_hypercube(p, args.k)
+                    
+                    diameter_val[mesh].append(estimate_diameter(graph))
+                    print(f"mesh {mesh} has diameter: {diameter_val[mesh][-1]} for this processor index: {i}")
+                    bisection_val[mesh].append(estimate_bisection_width(graph))
+                    print(f"mesh{mesh} has bisection: {bisection_val[mesh][-1]} for this processor index: {i}")
+
+
 
 
     
